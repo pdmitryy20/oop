4b9c769afa173f5eb99a4f19e0efdf9b12316990
pdmitryy20 <128388396+pdmitryy20@users.noreply.github.com>
2023-06-16 20:59:49 +0300

    Add files via upload

---
+++ lab5zavd3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lab5zavd3
+{
+    public class Shop
+    {
+        string[] clothingSizes = new string[] { "XXS", "XS", "S", "M", "L", "XL" };
+        public string getDescription(string size)
+        {
+            if (size == "XXS") return "Дитячий розмір";
+            else return "Дорослий розмір";
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shop shop = new Shop();
+            Console.Write("Введіть розмір одягу: ");
+            string size = Console.ReadLine();
+            string description = shop.getDescription(size);
+            Console.WriteLine(description);
+        }
+    }
+}
