6c715bb95515cbc12cc847bc82c5b5cbbffed9fe
pdmitryy20 <128388396+pdmitryy20@users.noreply.github.com>
2023-06-16 16:45:54 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static string ReplaceChars(string input, int position)
+        {
+            char[] charArray = input.ToCharArray();
+
+            for (int i = position; i < charArray.Length; i++)
+            {
+                if (charArray[i] == '0')
+                {
+                    charArray[i] = '1';
+                }
+                else if (charArray[i] == '1')
+                {
+                    charArray[i] = '0';
+                }
+            }
+            return new string(charArray);
+        }
+
+        static void Main(string[] args)
+        {
+            string input = "010101010101";
+            int postion = 2;
+            string result = ReplaceChars(input, postion);
+            Console.WriteLine("Результат: " + result);
+        }
+    }
+}
---
+++ lab10/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab10
+{
+    public class CharSet
+    {
+        private HashSet<char> set;
+        public CharSet()
+        {
+            set = new HashSet<char>();
+        }
+        public CharSet(string str)
+        {
+            set = new HashSet<char>(str);
+        }
+        public static CharSet operator +(CharSet set1, CharSet set2)
+        {
+            CharSet unionSet = new CharSet();
+            unionSet.set = new HashSet<char>(set1.set);
+            unionSet.set.UnionWith(set2.set);
+            return unionSet;
+        }
+        public override string ToString()
+        {
+            return string.Join(", ", set);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CharSet set1 = new CharSet("abc");
+            CharSet set2 = new CharSet("def");
+            CharSet set3 = new CharSet("abc");
+            CharSet unionSet = set1 + set2;
+            Console.WriteLine("Union: " + unionSet);
+        }
+    }
+}
